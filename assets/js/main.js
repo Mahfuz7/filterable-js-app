f6bfcdae386bba27ee3a44962804f35d88aa0a84
Mahfuz <m.mahfuz.me@gmail.com>
2017-07-23 12:42:02 +0600

    code refactored

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,34 @@
 /**
  * JavaScript Functionality
  */
+(function(){
 
-let filterInput = document.getElementById('filterInput');
+    let filterInput = document.getElementById('filterInput');
 
-filterInput.addEventListener('keyup', filterNames );
+    filterInput.addEventListener('keyup', filterNames );
 
-function filterNames() {
-    let filterValue = document.getElementById('filterInput').value.toUpperCase();
+    function filterNames() {
 
-    // Get names ul
-    let ul = document.getElementById('names');
+        let filterValue = document.getElementById('filterInput').value.toUpperCase();
 
-    // Get lis from ul
-    let li = ul.querySelectorAll('li.collection-item');
+        // Get names ul
+        let ul = document.getElementById('names');
 
-    // Loop through collection-item li
-    for( i = 0; i < li.length; i++ ) {
-        let a = li[i].getElementsByTagName('a')[0];
+        // Get lis from ul
+        let li = ul.querySelectorAll('li.collection-item');
 
-        if( a.innerHTML.toUpperCase().indexOf(filterValue) > -1 ) {
-            li[i].style.display = '';
-        }else {
-            li[i].style.display = 'none';
+        // Loop through collection-item li
+        for( i = 0; i < li.length; i++ ) {
+            let a = li[i].getElementsByTagName('a')[0];
+
+            if( a.innerHTML.toUpperCase().indexOf(filterValue) > -1 ) {
+                li[i].style.display = '';
+            }else {
+                li[i].style.display = 'none';
+            }
         }
+
     }
 
-}
+
+})();
